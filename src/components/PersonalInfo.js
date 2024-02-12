fdb588ef9b056da8cde58887229585a7ae3638c6
RJCB <37097058+RJCB@users.noreply.github.com>
2024-02-12 18:22:50 -0500

    update company name to Dayforce

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,39 +1,95 @@
-import React from 'react';
+import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const PersonalInfo = () => {
-    const FL_List = ["HTML5", "CSS3", "SASS", "Stylus", "BOOTSTRAP", "TAILWIND", "JAVASCRIPT ES6+", "EMBER JS", "REACT JS", "Dojo Toolkit", "VUE JS", "REDUX", "HANDLEBARS", "CYPRESS", "QUNIT", "AGILE"];
-    const VC_List = ["GIT", "GITHUB", "Azure DevOps Repos", "BitBucket"];
-    const ALLY_List = ["AXE", "NVDA", "VOICEOVER"];
+  const FL_List = [
+    "HTML5",
+    "CSS3",
+    "SASS",
+    "Stylus",
+    "BOOTSTRAP",
+    "TAILWIND",
+    "JAVASCRIPT ES6+",
+    "EMBER JS",
+    "REACT JS",
+    "Dojo Toolkit",
+    "VUE JS",
+    "REDUX",
+    "HANDLEBARS",
+    "JEST",
+    "CYPRESS",
+    "QUNIT",
+    "AGILE",
+  ];
+  const VC_List = ["GIT", "GITHUB", "Azure DevOps Repos", "BitBucket"];
+  const ALLY_List = ["AXE", "NVDA", "VOICEOVER"];
 
-    const FLelements = FL_List.map((list, index) => {
-        return <span key={list + index} className="skill">{list}</span>
-    })
-
-    const VCelements = VC_List.map((list, index) => {
-        return <span key={list + index} className="skill">{list}</span>
-    })
-
-    const ALLYelements = ALLY_List.map((list, index) => {
-        return <span key={list + index} className="skill">{list}</span>
-    })
-
+  const FLelements = FL_List.map((list, index) => {
     return (
-        <div className="personal-info">
-            <h1>Rajesh B</h1>
-            <h2>Front-End Web Developer</h2>
-            <p className="personal-info_description">Hi, I am a web developer based in Toronto. I enjoy building interactive web applications.</p>
-            <div className="skills">
-                <p><span className="skill-heading">Frameworks and Libraries:</span><span>{FLelements}</span> </p>
-                <p><span className="skill-heading">Version control:</span> <span>{VCelements}</span></p>
-                <p><span className="skill-heading">Web Accessibility:</span> <span>{ALLYelements}</span></p>
-            </div>
-            <div className="personal-info_icons">
-                <a href="https://github.com/RJCB" rel="noreferrer" target="_blank"><span><FaGithub /></span><span>Github</span></a>
-                <a href="https://www.linkedin.com/in/rajesh-b-a2ab92198/" rel="noreferrer noopener" target="_blank"><span><FaLinkedin /></span><span>LinkedIn</span></a>
-            </div>
-        </div>
-    )
-}
+      <span key={list + index} className="skill">
+        {list}
+      </span>
+    );
+  });
 
-export default PersonalInfo
+  const VCelements = VC_List.map((list, index) => {
+    return (
+      <span key={list + index} className="skill">
+        {list}
+      </span>
+    );
+  });
+
+  const ALLYelements = ALLY_List.map((list, index) => {
+    return (
+      <span key={list + index} className="skill">
+        {list}
+      </span>
+    );
+  });
+
+  return (
+    <div className="personal-info">
+      <h1>Rajesh B</h1>
+      <h2>Front-End Web Developer</h2>
+      <p className="personal-info_description">
+        Hi, I am a web developer based in Toronto. I enjoy building interactive
+        web applications.
+      </p>
+      <div className="skills">
+        <p>
+          <span className="skill-heading">Frameworks and Libraries:</span>
+          <span>{FLelements}</span>{" "}
+        </p>
+        <p>
+          <span className="skill-heading">Version control:</span>{" "}
+          <span>{VCelements}</span>
+        </p>
+        <p>
+          <span className="skill-heading">Web Accessibility:</span>{" "}
+          <span>{ALLYelements}</span>
+        </p>
+      </div>
+      <div className="personal-info_icons">
+        <a href="https://github.com/RJCB" rel="noreferrer" target="_blank">
+          <span>
+            <FaGithub />
+          </span>
+          <span>Github</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rajesh-b-a2ab92198/"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          <span>
+            <FaLinkedin />
+          </span>
+          <span>LinkedIn</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
--- src/components/ProfessionalInfo.js
+++ src/components/ProfessionalInfo.js
@@ -1,101 +1,151 @@
-import React from 'react'
-import { Card, CardGroup } from 'react-bootstrap'
+import React from "react";
+import { Card, CardGroup } from "react-bootstrap";
 
 const ProfessionalInfo = () => {
-    return (
-        <div className="professional-info">
-            <div className="experience">
-                <h2>WORK EXPERIENCE</h2>
-                <CardGroup style={{ flexDirection: "column" }}>
-                    <Card body>
-                        <Card.Title as="h3">Ceridian HCM</Card.Title>
-                        <h4>Front-End developer<span>June 2022 - present</span></h4>
-                        <p className="techstack"><span>HTML, CSS, JS ES6+, Dojo Toolkit, React JS, Redux, Sass, Stylus, Git, Azure DevOps Repos</span></p>
-                        <div className="role">
-                            <p>Build, maintain and remediate Dayforce application in Dojo Toolkit and React JS</p>
-                        </div>
-                    </Card>
-                    <Card body>
-                        <Card.Title as="h3">CIBC</Card.Title>
-                        <h4>Front-End developer<span>May 2019 - June 2022</span></h4>
-                        <p className="techstack"><span>HTML, CSS, JS ES6+, Ember JS, Vue JS, React JS, Redux, Handlebars, Sass, Git, GitHub</span></p>
-                        <div className="role">
-                            <p>Build and maintain CIBC and Simplii banking applications</p>
-                        </div>
-                    </Card>
-                    <Card body>
-                        <h3>Kontron</h3>
-                        <h4>Front-End developer<span>Nov 2017 - May 2019</span></h4>
-                        <p className="techstack"><span>HTML, CSS, JS ES6+, React JS, Redux, Sass, Git, GitHub</span></p>
-                        <div className="role">
-                            <p>Build and maintain test results related internal application</p>
-                        </div>
-                    </Card>
-                    <Card body>
-                        <h3>Flir systems</h3>
-                        <h4>Front-End developer<span>Mar 2017 - Nov 2017</span></h4>
-                        <p className="techstack"><span>HTML, CSS, JS ES6+, Sass, React JS, Redux, BitBucket</span></p>
-                        <div className="role">
-                            <p>Build and maintain Flir systems web application and an internal product application</p>
-                        </div>
-                    </Card>
-                    {/* <Card body>
-                        <h3>Paragon Systems</h3>
-                        <h4>Web developer<span>Apr 2016 - Jan 2017</span></h4>
-                        <p className="techstack"><span>HTML, CSS, JavaScript, Angular JS</span></p>
-                        <div className="role">
-                            <p>Build and maintain an Internal application</p>
-                            <p>Design FE part of the application</p>
-                        </div>
-                    </Card> */}
-                </CardGroup>
+  return (
+    <div className="professional-info">
+      <div className="experience">
+        <h2>WORK EXPERIENCE</h2>
+        <CardGroup style={{ flexDirection: "column" }}>
+          <Card body>
+            <Card.Title as="h3">Dayforce HCM</Card.Title>
+            <h4>
+              Senior Software developer<span>June 2022 - present</span>
+            </h4>
+            <p className="techstack">
+              <span>
+                HTML, CSS, JS ES6+, React JS, Redux, Dojo Toolkit, Sass, Stylus,
+                Git, Azure DevOps Repos
+              </span>
+            </p>
+            <div className="role">
+              <p>
+                Build, maintain and remediate Dayforce application in Dojo
+                Toolkit and React JS
+              </p>
             </div>
-            <div className="projects">
-                <h2>PERSONAL PROJECTS</h2>
-                <CardGroup style={{ flexDirection: "column" }}>
-                    <Card body>
-                        <Card.Title as="h3">The Movie Database</Card.Title>
-                        <p className="techstack"><span>HTML, CSS, React JS, Sass</span></p>
-                        <div className="role">
-                            <p>Search and view list of movies and tv shows</p>
-                        </div>
-                        <a href="https://github.com/RJCB/react-movie-db-app" rel="noreferrer noopener" target="_blank">Click to view on GitHub</a>
-                    </Card>
-                    <Card body>
-                        <Card.Title as="h3">Crypto Tracker</Card.Title>
-                        <p className="techstack"><span>HTML, CSS, React JS, Sass, Chart JS</span></p>
-                        <div className="role">
-                            <p>Search and view list of Crypto currencies and their value change chart</p>
-                        </div>
-                        <a href="https://github.com/RJCB/react-crypto-tracker" rel="noreferrer noopener" target="_blank">Click to view on GitHub</a>
-                    </Card>
-                    <Card body>
-                        <Card.Title as="h3">Youtube clone</Card.Title>
-                        <p className="techstack"><span>HTML, CSS, React JS, Redux, Sass, Firebase</span></p>
-                        <div className="role">
-                            <p>Youtube clone</p>
-                        </div>
-                        <a href="https://github.com/RJCB/react-youtube-clone" rel="noreferrer noopener" target="_blank">Click to view on GitHub</a>
-                    </Card>
-                    <Card body>
-                        <a href="https://github.com/RJCB" rel="noreferrer noopener" target="_blank">... view more on GitHub</a>
-                    </Card>
-                </CardGroup>
+          </Card>
+          <Card body>
+            <Card.Title as="h3">CIBC</Card.Title>
+            <h4>
+              Front-End developer<span>May 2019 - June 2022</span>
+            </h4>
+            <p className="techstack">
+              <span>
+                HTML, CSS, JS ES6+, Ember JS, Vue JS, React JS, Redux,
+                Handlebars, Sass, Git, GitHub
+              </span>
+            </p>
+            <div className="role">
+              <p>Build and maintain CIBC and Simplii banking applications</p>
             </div>
-            <div className="education">
-                <h2>EDUCATION and CERTIFICATIONS</h2>
-                <CardGroup style={{ flexDirection: "column" }}>
-                    <Card body className="tmdb">
-                        <Card.Title as="h3">Bachelors degree in Electronics and communications engineering - 2012</Card.Title>
-                        <p className="techstack"><span>JNTU - Hyderabad, India</span></p>
-                        <Card.Title as="h3">Electromechanical engineering technologist - 2015</Card.Title>
-                        <p className="techstack"><span>George Brown College - Toronto</span></p>
-                        <Card.Title as="h3">ITIL - 2018</Card.Title>
-                    </Card>
-                </CardGroup>
+          </Card>
+          <Card body>
+            <h3>Kontron</h3>
+            <h4>
+              Front-End developer<span>Nov 2017 - May 2019</span>
+            </h4>
+            <p className="techstack">
+              <span>
+                HTML, CSS, JS ES6+, React JS, Redux, Sass, Git, GitHub
+              </span>
+            </p>
+            <div className="role">
+              <p>
+                Build and maintain test results related internal application
+              </p>
             </div>
-        </div>
-    )
-}
+          </Card>
+        </CardGroup>
+      </div>
+      <div className="projects">
+        <h2>PERSONAL PROJECTS</h2>
+        <CardGroup style={{ flexDirection: "column" }}>
+          <Card body>
+            <Card.Title as="h3">The Movie Database</Card.Title>
+            <p className="techstack">
+              <span>HTML, CSS, React JS, Sass</span>
+            </p>
+            <div className="role">
+              <p>Search and view list of movies and tv shows</p>
+            </div>
+            <a
+              href="https://github.com/RJCB/react-movie-db-app"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Click to view on GitHub
+            </a>
+          </Card>
+          <Card body>
+            <Card.Title as="h3">Crypto Tracker</Card.Title>
+            <p className="techstack">
+              <span>HTML, CSS, React JS, Sass, Chart JS</span>
+            </p>
+            <div className="role">
+              <p>
+                Search and view list of Crypto currencies and their value change
+                chart
+              </p>
+            </div>
+            <a
+              href="https://github.com/RJCB/react-crypto-tracker"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Click to view on GitHub
+            </a>
+          </Card>
+          <Card body>
+            <Card.Title as="h3">Youtube clone</Card.Title>
+            <p className="techstack">
+              <span>HTML, CSS, React JS, Redux, Sass, Firebase</span>
+            </p>
+            <div className="role">
+              <p>Youtube clone</p>
+            </div>
+            <a
+              href="https://github.com/RJCB/react-youtube-clone"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              Click to view on GitHub
+            </a>
+          </Card>
+          <Card body>
+            <a
+              href="https://github.com/RJCB"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              ... view more on GitHub
+            </a>
+          </Card>
+        </CardGroup>
+      </div>
+      <div className="education">
+        <h2>EDUCATION and CERTIFICATIONS</h2>
+        <CardGroup style={{ flexDirection: "column" }}>
+          <Card body className="tmdb">
+            <Card.Title as="h3">
+              Bachelors degree in Electronics and communications engineering -
+              2013
+            </Card.Title>
+            <p className="techstack">
+              <span>JNTU - Hyderabad, India</span>
+            </p>
+            <Card.Title as="h3">
+              Electromechanical engineering technologist - 2015
+            </Card.Title>
+            <p className="techstack">
+              <span>George Brown College - Toronto</span>
+            </p>
+            <Card.Title as="h3">ITIL - 2018</Card.Title>
+          </Card>
+        </CardGroup>
+      </div>
+    </div>
+  );
+};
 
-export default ProfessionalInfo
+export default ProfessionalInfo;
